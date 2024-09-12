93043608355d1e8522eac9fb8d7efa610f3bcaa9
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-09-12 18:15:20 +0400

    login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Req } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { Response,Request } from 'express';
 import { Public } from './decorators/public.decorator';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('/login')
   Login(@Body() data: CreateAuthDto) {
-    return this.authService.Login(data)
+    return this.authService.Login(data);
   }
 }
