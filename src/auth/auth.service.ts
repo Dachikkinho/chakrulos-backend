4503ce982da5afc83ab251514a8adf6b489d7359
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-09-13 14:11:04 +0400

    user-block

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,41 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UsersRepository } from 'src/users/users.repository';
-import * as Bcrypt from 'bcrypt'
+import * as Bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersRepo: UsersRepository, private readonly jwtService: JwtService) { }
+  constructor(
+    private readonly usersRepo: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async Login(data: CreateAuthDto) {
-    let user = await this.usersRepo.findUserByEmail(data.email)
+    let user = await this.usersRepo.findUserByEmail(data.email);
 
     if (!user) {
-      throw new UnauthorizedException('access denied')
+      throw new UnauthorizedException('access denied');
     }
 
-    const IsPasswordCorrect = await Bcrypt.compare(data.password, user.password)
+    const IsPasswordCorrect = await Bcrypt.compare(
+      data.password,
+      user.password,
+    );
 
     if (!IsPasswordCorrect) {
-      throw new UnauthorizedException('access denied')
+      throw new UnauthorizedException('access denied');
+    }
+
+    if (user.blocked) {
+      throw new UnauthorizedException('USER IS BLOCKED');
     }
 
     const jwtToken = await this.jwtService.signAsync({
       id: user.id,
-      role: user.role
-    })
+      role: user.role,
+    });
 
-    return jwtToken
+    return jwtToken;
   }
 }
