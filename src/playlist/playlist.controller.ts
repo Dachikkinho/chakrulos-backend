5c718f1dc5a4ed613fdd1992bcdd3e2a481368ad
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-09-14 15:55:29 +0400

    Navi/Playlist-Fix

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
 import { CreatePlaylistDto } from './dto/create-playlist.dto';
 import { UpdatePlaylistDto } from './dto/update-playlist.dto';
@@ -6,36 +16,39 @@ import { RoleEnum } from 'src/auth/roles/roles.enum';
 import { Roles } from 'src/auth/decorators/public.decorator';
 import { Request } from 'express';
 
-@Roles(RoleEnum.admin)
+@Roles(RoleEnum.user, RoleEnum.admin)
 @Controller('playlist')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
 
   @Get('/user')
-  findOneUsersAllPlayList(@Query('id') id: string){
+  findOneUsersAllPlayList(@Query('id') id: string) {
     return this.playlistService.findOneUsersAllPlayList(+id);
   }
 
   @Post()
-  @Roles(RoleEnum.user, RoleEnum.admin)
-  create(@Body() createPlaylistDto: CreatePlaylistDto, @Req() request: Request) {
+  create(
+    @Body() createPlaylistDto: CreatePlaylistDto,
+    @Req() request: Request,
+  ) {
     return this.playlistService.create(createPlaylistDto, request);
   }
 
   @Get()
-  @Roles(RoleEnum.user, RoleEnum.admin)
   findAll() {
     return this.playlistService.findAll();
   }
 
   @Get(':id')
-  @Roles(RoleEnum.user, RoleEnum.admin)
   findOne(@Param('id') id: string) {
     return this.playlistService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlaylistDto: UpdatePlaylistDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePlaylistDto: UpdatePlaylistDto,
+  ) {
     return this.playlistService.update(+id, updatePlaylistDto);
   }
 
@@ -44,7 +57,6 @@ export class PlaylistController {
     return this.playlistService.remove(+id);
   }
 
-
   @Delete(':playlistId/music/:musicId')
   async removeMusic(
     @Param('playlistId') playlistId: number,
@@ -52,5 +64,4 @@ export class PlaylistController {
   ) {
     return this.playlistService.removeMusicFromPlaylist(playlistId, musicId);
   }
-
 }
